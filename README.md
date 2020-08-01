ffa5217cd5c0d60dd88e960427396383ad78c339
nitagr <nitishagrahari12@gmail.com>
2020-08-01 22:10:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nitish19
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nitish19)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'The Book Thief',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Art of Not Giving a Fuck',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Learning C++',
+    price: 899,
+    description: ''
+  },
+  {
+    name: 'Leaning Angular',
+    price: 799,
+    description: 'this is my lenovo laptop'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
